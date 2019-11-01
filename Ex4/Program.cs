834bd99502660b6a63819cdd32b55d26b254ef1b
Bogdan <Bogdan@DESKTOP-TN1VCHG>
2019-11-01 15:00:10 +0200

    Add project files.

---
+++ Ex4/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Professor p1 = new Professor
+            {
+                Name = "Asd Dsa",
+                Faculty = "ETTI",
+                Specialization = "inginer"
+
+            };
+
+            Professor p2 = new Professor
+            {
+                Name = "bls sds",
+                Faculty = "AC",
+                Specialization = "informatician"
+            };
+
+            Student s1 = new Student
+            {
+                Name = "Ion",
+                Faculty = "ETTI",
+                Year = 1
+            };
+
+
+            Student s2 = new Student
+            {
+                Name = "Vasile",
+                Faculty = "ETTI",
+                Year = 1
+            };
+
+            Student s3 = new Student
+            {
+                Name = "Csa",
+                Faculty = "AC",
+                Year = 2
+
+            };
+
+            Student s4 = new Student
+            {
+                Name = "Kjl",
+                Faculty = "AC",
+                Year = 2
+            };
+
+            Course c1 = new Course
+            {
+                Name = "C1",
+                Profesor = p1,
+                Year = 1,
+
+
+
+            };
+            c1.AddStd(s1.Name);
+            c1.AddStd(s2.Name);
+
+            Course c2 = new Course
+            {
+                Name = "C2",
+                Profesor = p2,
+                Year = 2
+
+            };
+            c2.AddStd(s3.Name);
+            c2.AddStd(s4.Name);
+
+            c1.Print();
+            Console.WriteLine();
+            c2.Print();
+            Console.ReadLine();
+        }
+    }
+
+
+
+    class Professor
+    {
+        public string Name { get; set; }
+        public string Faculty { get; set; }
+        public string Specialization { get; set; }
+        public void Print()
+        {
+            Console.WriteLine($"Nume:{Name}, faculty:{Faculty}, specialization:{Specialization}");
+        }
+    }
+
+    class Student
+    {
+        public string Name { get; set; }
+        public string Faculty { get; set; }
+        public int Year { get; set; }
+        public void Print()
+        {
+            Console.WriteLine($"Student:{Name}, Faculty:{Faculty}, Year:{Year}");
+        }
+
+
+    }
+
+    class Course
+    {
+        List<string> listastudenti = new List<string>();
+
+        public string Name { get; set; }
+        public Professor Profesor { get; set; }
+        public int Year { get; set; }
+        public void AddStd(string s)
+        {
+            listastudenti.Add(s);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Numele cursului: {Name}, Year: {Year}");
+            Profesor.Print();
+            Console.WriteLine("Lista studenti: ");
+            for (int i = 0; i < listastudenti.Count; i++)
+            {
+                Console.WriteLine(listastudenti[i]);
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Pizza/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PizzaApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PizzaBase b1 = new PizzaBase { Name = "Regular", Cost = 2 };
+            PizzaBase b2 = new PizzaBase { Name = "Thick", Cost = 3 };
+            PizzaBase b3 = new PizzaBase { Name = "Italian", Cost = 1.5 * b1.Cost };
+
+            PizzaTopping t1 = new PizzaTopping { Name = "Cheese", Cost = 1.4 };
+            PizzaTopping t2 = new PizzaTopping { Name = "SUNCA", Cost = 2 };
+            PizzaTopping t3 = new PizzaTopping { Name = "Ardei", Cost = 3 };
+
+            Pizza p1 = new Pizza { Name = "Quatro", Baza = b1 };
+            p1.AddTop(t1);
+            p1.AddTop(t2);
+            p1.Print();
+
+            Console.WriteLine();
+
+            Pizza p2 = new Pizza { Name = "Full", Baza = b3 };
+            p2.AddTop(t1);
+            p2.AddTop(t2);
+            p2.AddTop(t3);
+            p2.Print();
+
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+
+    class PizzaBase
+    {
+        public string Name { get; set; }
+        public double Cost { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine($"Blat: {Name} (${Cost})");
+        }
+    }
+
+    class PizzaTopping
+
+    {
+        public string Name { get; set; }
+        public double Cost { get; set; }
+
+
+
+        public void Print()
+        {
+            Console.WriteLine($"{Name} (${Cost})");
+        }
+
+
+    }
+    class Pizza
+    {
+        public string Name { get; set; }
+        public PizzaBase Baza { get; set; }
+        List<PizzaTopping> lista = new List<PizzaTopping>();
+
+        public void AddTop(PizzaTopping top)
+        {
+
+            lista.Add(top);
+        }
+
+        public void Print()
+        {
+            double sum = 0;
+            Console.WriteLine($"Pizza {Name}");
+            Baza.Print();
+            Console.WriteLine("Toppings:");
+            for (int i = 0; i < lista.Count; i++)
+            {
+
+                lista[i].Print();
+                sum += lista[i].Cost;
+            }
+            double cost = sum + Baza.Cost;
+            Console.WriteLine($"Total Cost: ${cost}");
+        }
+
+    }
+
+}
